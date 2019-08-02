06352cfdb6398995664f21592d68f1562ba62e40
AAMIR SHAHZAD <aamir.shahzad1@ibm.com>
2019-08-02 10:37:13 +0500

    webscraping source code added

---
+++ WebScraper/demo4.js
@@ -0,0 +1,26 @@
+
+
+const demo4 = function(){
+    const puppeteer = require('puppeteer');
+    const url = 'https://www.reddit.com';
+    
+    puppeteer
+      .launch()
+      .then(function(browser) {
+        return browser.newPage();
+      })
+      .then(function(page) {
+        return page.goto(url).then(function() {
+          return page.content();
+        });
+      })
+      .then(function(html) {
+        console.log(html);
+      })
+      .catch(function(err) {
+        //handle error
+      });
+
+}
+
+module.exports = demo4
