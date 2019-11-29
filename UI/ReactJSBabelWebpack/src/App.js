8dffb0d8dce7f3bbe1fad07d20f5e370466e6725
Aamir Shahzad <53485712+aamir-shahzad1@users.noreply.github.com>
2019-11-29 21:02:59 +0500

    reactJSMaterialUIBabelWebpackRedux Code Added

--- UI/ReactJSBabelWebpack/src/App.js
+++ UI/ReactJSBabelWebpack/src/App.js
@@ -1,31 +1,6 @@
 import { useState } from 'react';
 import React, { Component } from 'react';
-import SimpleForm from './components/SimpleForm';
-import './App.css';
-
-// const App = () => {
-//   let [showChat, setShowChat] = useState(false);
-
-//   const startChat = () => { setShowChat(true); }
-//   const hideChat = () => { setShowChat(false); }
-
-//     return (
-//       <div>Testing</div>
-//       // <div className = "bot">
-//       //   <div>Testing it </div>
-//       //   <div style ={{display: showChat ? "" : "none"}}>
-//       //     <SimpleForm></SimpleForm>
-//       //   </div>      
-//       //   {/* <div> {showChat ? <SimpleForm></SimpleForm> : null} </div> */}
-//       //   <div>
-//       //     {!showChat 
-//       //       ? <button className="btn" onClick={() => startChat()}>click to chat... </button> 
-//       //       : <button className="btn" onClick={() => hideChat()}>click to hide... </button>}
-//       //   </div>
-//       // </div>      
-//     )
-// }
-
+import Admin from './components/admin/Admin'
 class App extends Component {
   constructor(props) {
     super(props);
@@ -34,25 +9,12 @@ class App extends Component {
       setShowChat: false
     };
   }
-  // let [showChat, setShowChat] = useState(false);
-
-  // startChat = () => { this.setState({ setShowChat: true }) }
-  // hideChat = () => { this.setState({ setShowChat: false }) }
   render() {
     return (
-      
-      // <div className="bot">
-      //   <div>Testing it </div>
-        <div style={{ display: this.state.showChat ? "" : "none" }}>
-          <SimpleForm></SimpleForm>
+      <div>
+        <h1>I am App</h1>
+        <Admin />
         </div>
-      //   {/* <div> {showChat ? <SimpleForm></SimpleForm> : null} </div> */}
-      //   <div>
-      //     {!showChat
-      //       ? <button className="btn" onClick={this.startChat}>click to chat... </button>
-      //       : <button className="btn" onClick={this.hideChat}>click to hide... </button>}
-      //   </div>
-      // </div>
     )
   }
 }
