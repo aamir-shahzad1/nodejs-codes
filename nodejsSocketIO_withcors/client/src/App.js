54f054305d633bc0b5bcbbe5e76d37f9d796bde8
Aamir Shahzad <53485712+aamir-shahzad1@users.noreply.github.com>
2019-12-04 21:54:32 +0500

    server/client modified to ger

--- nodejsSocketIO_withcors/client/src/App.js
+++ nodejsSocketIO_withcors/client/src/App.js
@@ -9,16 +9,17 @@ class App extends Component {
       endpoint: "localhost:4001",
 
       ///
-      color: 'white'
+      color: 'white',
       ///
-
+      socket: socketIOClient("localhost:4001")
     };
   }
+  
 
   // sending sockets
   send = () => {
-    const socket = socketIOClient(this.state.endpoint);
-    socket.emit('change color', this.state.color) // change 'red' to this.state.color
+    // const socket = socketIOClient(this.state.endpoint);
+    this.state.socket.emit('change color', this.state.color) // change 'red' to this.state.color
     console.log('change color is called')
   }
   ///
@@ -30,9 +31,9 @@ class App extends Component {
   }
 
   componentDidMount = () => {
-      const socket = socketIOClient(this.state.endpoint);
+      // const socket = socketIOClient(this.state.endpoint);
       setInterval(this.send(), 1000)
-      socket.on('change color', (col) => {
+      this.state.socket.on('change color', (col) => {
           document.body.style.backgroundColor = col
       })
   }
@@ -40,7 +41,6 @@ class App extends Component {
   render() {
     // testing for socket connections
 
-    const socket = socketIOClient(this.state.endpoint);
 
     return (
       <div style={{ textAlign: "center" }}>
--- nodejsSocketIO_withcors/server.js
+++ nodejsSocketIO_withcors/server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const http = require('http')
 const socketIO = require('socket.io')
+const pf = require('pretty-format')
 
 
 var bodyParser = require('body-parser'); // parser for post requests
@@ -19,13 +20,20 @@ io.set('origins', '*:*');
 // This is what the socket.io syntax is like, we will work this later
 io.on('connection', socket => {
   console.log('New client connected')
+   console.log('=============== START ================')
+  //  console.log(pf(io.sockets))
+   console.log(pf( Object.keys( (socket.server['eio']).clients ))) 
 
-  // just like on the client side, we have a socket.on method that takes a callback function
+  console.log('=============== END ================')
+   
+   // just like on the client side, we have a socket.on method that takes a callback function
   socket.on('change color', (color) => {
     // once we get a 'change color' event from one of our clients, we will send it to the rest of the clients
     // we make use of the socket.emit method again with the argument given to use from the callback function above
+    console.log('My ID : ', socket.id)
     console.log('Color Changed to: ', color)
     io.sockets.emit('change color', color)
+    // socket.broadcast.to(socketid).emit('message', 'for your eyes only');
   })
 
   // disconnect is fired when a client leaves the server
@@ -33,3 +41,5 @@ io.on('connection', socket => {
     console.log('user disconnected')
   })
 })
+
+
